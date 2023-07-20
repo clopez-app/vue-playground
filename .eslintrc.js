40bd821e7f87a457b470f202f56bce7fc8866a71
Carlos Lopez <clopez@applaudostudios.com>
2023-07-20 15:27:20 -0600

    add tools and configuration for pre-push hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,4 +10,9 @@ module.exports = {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
 };
