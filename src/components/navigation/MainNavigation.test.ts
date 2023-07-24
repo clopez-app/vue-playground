54f7e3d5236dc35ca71dfa76bdb1a2f0eb8e8589
Carlos Lopez <clopez@applaudostudios.com>
2023-07-24 11:34:10 -0600

    add test for new components and configurations

---
+++ src/components/navigation/MainNavigation.test.ts
@@ -0,0 +1,23 @@
+import MainNavigation from './MainNavigation.vue';
+import { mount } from '@vue/test-utils';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from '../../routes';
+
+test('Main Navigation works', () => {
+  expect(MainNavigation).toBeTruthy();
+
+  const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+  });
+
+  const wrapper = mount(MainNavigation, {
+    global: {
+      plugins: [router],
+    },
+  });
+
+  expect(wrapper.findAll('a').length).toBe(2);
+  expect(wrapper.findAll('a')[0].text()).toContain('Home');
+  expect(wrapper.findAll('a')[1].text()).toContain('About');
+});
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
-import AboutContainer from './components/about/AboutContainer.vue';
-
-const routes = [
-  { path: '/', component: HelloWorld },
-  { path: '/about', component: AboutContainer },
-];
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from './routes';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
